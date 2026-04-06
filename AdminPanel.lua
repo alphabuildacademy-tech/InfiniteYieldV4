3fd82c81a01914ae60602d74bcf20de251a6e719
alphabuildacademy-tech <alphabuild.academy@gmail.com>
2026-04-06 18:38:48 -0400

    Updated admin panel

--- AdminPanel.lua
+++ AdminPanel.lua
@@ -2,37 +2,53 @@ local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
     Name = "Infinite Yield V4 - Admin Panel",
-    ConfigurationSaving = {Enabled = false},
+    ConfigurationSaving = {
+        Enabled = false
+    },
     KeySystem = false,
     ToggleUIKeybind = Enum.KeyCode.RightControl,
 })
 
-local LocalPlayer = game.Players.LocalPlayer
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
 local Character = LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()
 local Humanoid = Character:WaitForChild("Humanoid")
 
 local MainTab = Window:CreateTab("Player", 0)
 local Section = MainTab:CreateSection("Stats")
 
-Section:CreateSlider({
+local SpeedSlider = Section:CreateSlider({
     Name = "Walk Speed",
     Range = {16, 250},
     Increment = 1,
+    Suffix = "Speed",
     CurrentValue = Humanoid.WalkSpeed,
-    Flag = "Speed",
+    Flag = "SpeedSlider",
     Callback = function(Value)
         Humanoid.WalkSpeed = Value
     end
 })
 
-Section:CreateSlider({
+local JumpSlider = Section:CreateSlider({
     Name = "Jump Power",
     Range = {50, 250},
     Increment = 1,
+    Suffix = "Power",
     CurrentValue = Humanoid.JumpPower,
-    Flag = "Jump",
+    Flag = "JumpSlider",
     Callback = function(Value)
         Humanoid.JumpPower = Value
+    end
+})
+
+Section:CreateButton({
+    Name = "Example Button",
+    Callback = function()
+        Rayfield:Notify({
+            Title = "Button Clicked",
+            Content = "The button works!",
+            Duration = 2
+        })
     end
 })
 
